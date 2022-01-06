6dc33024323885b16066852c131b7da294eb2ac9
itinerare <ad@itinerare.net>
2022-01-05 20:45:56 -0500

    fix(items): switch to cleaner rarity fetching - tested on recent mariaDB; should be overall better/more consistent

--- resources/views/inventory/_raritybox.blade.php
+++ resources/views/inventory/_raritybox.blade.php
@@ -5,7 +5,7 @@
         <p>This item can be opened to obtain an item with the rarity <strong>{{ $tag->data }}</strong>. Please note that you can only select one item each time you open this box, so if you have multiple and want different items, you should open them one at a time. This action is not reversible. Are you sure you want to open this box?</p>
         <div class="form-group">
             {!! Form::label('item_id', 'Item') !!}
-            {!! Form::select('item_id', App\Models\Item\Item::released()->whereNotNull('data')->whereRaw('JSON_EXTRACT(`data`, \'$.rarity\') = ' . $tag->data)->orderBy('name')->pluck('name','id')->toArray(), null, ['class' => 'form-control rarityBox selectize', 'placeholder' => 'Select an Item']) !!}
+            {!! Form::select('item_id', App\Models\Item\Item::released()->whereNotNull('data')->where('data->rarity', $tag->data)->orderBy('name')->pluck('name','id')->toArray(), null, ['class' => 'form-control rarityBox selectize', 'placeholder' => 'Select an Item']) !!}
         </div>
         <script>
             $(document).ready(function() {
