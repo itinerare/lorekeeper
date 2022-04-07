bd2a07e7161b7ae0f41bd87004f387e897afb6bd
itinerare <ad@itinerare.net>
2022-04-07 15:01:09 -0400

    chore: update ext tracker info

---
+++ config/lorekeeper/ext-tracker/sellablecharacters.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'key' => 'sellablecharacters',
+    'wiki_key' => 'Sellable_Characters',
+    'creators' => json_encode([
+        'Ne-wt' => 'https://github.com/Ne-wt/',
+    ]),
+    'version' => '1.5.0',
+];
+
