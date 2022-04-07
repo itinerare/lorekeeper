740375882c6098e7c4ec38b4f9a47447fbcb4e72
itinerare <ad@itinerare.net>
2022-04-07 14:48:25 -0400

    fix(characters): fix surrender page formatting to be more responsive

--- resources/views/admin/surrenders/surrender.blade.php
+++ resources/views/admin/surrenders/surrender.blade.php
@@ -43,10 +43,10 @@
     <div class="card mb-3">
         <div class="card-body">
             <div class="row">
-                <div class="col-md-2 mb-md-0 mb-2 mr-3">
-                <img src="{{ $surrender->character->image->thumbnailurl }}"> 
+                <div class="col-sm-4">
+                <img src="{{ $surrender->character->image->thumbnailurl }}">
                 </div>
-                <div class="col-md-2 mb-md-0 mb-2">
+                <div class="col-sm-4">
                     <a href="{{ $surrender->character->url }}"><h3 class="text-uppercase">{!! $surrender->character->displayname !!}</h3></a>
                         <strong>Species</strong> - {!! $surrender->character->image->species->displayname !!}<br>
                         <strong>Subtype</strong> - {!! $surrender->character->image->subtype_id ? $surrender->character->image->subtype->displayName : 'None' !!}<br>
@@ -55,13 +55,13 @@
                         <?php $features = $surrender->character->image->features()->with('feature.category')->get(); ?>
                     @if($features->count())
                             @foreach($features as $feature)
-                                <div>@if($feature->feature->feature_category_id) <strong>{!! $feature->feature->category->displayName !!}:</strong> @endif {!! $feature->feature->displayName !!} @if($feature->data) ({{ $feature->data }}) @endif</div> 
+                                <div>@if($feature->feature->feature_category_id) <strong>{!! $feature->feature->category->displayName !!}:</strong> @endif {!! $feature->feature->displayName !!} @if($feature->data) ({{ $feature->data }}) @endif</div>
                             @endforeach
-                    @else 
+                    @else
                             <div>No traits listed.</div>
                     @endif
                 </div>
-                <div class="col-md-6 mb-md-0 mb-2">
+                <div class="col-sm-4">
                     <h5>User Suggested worth:</h5>
                     {{ $surrender->worth }} @if($surrender->worth) {{ $worth->name }} @endif
                     <br>
@@ -134,10 +134,10 @@
 @endsection
 
 @section('scripts')
-@parent 
+@parent
 @if($surrender->status == 'Pending')
     <script>
-        
+
         $(document).ready(function() {
             var $confirmationModal = $('#confirmationModal');
             var $surrenderForm = $('#surrenderForm');
@@ -149,14 +149,14 @@
             var $rejectionButton = $('#rejectionButton');
             var $rejectionContent = $('#rejectionContent');
             var $rejectionSubmit = $('#rejectionSubmit');
-            
+
             $approvalButton.on('click', function(e) {
                 e.preventDefault();
                 $approvalContent.removeClass('hide');
                 $rejectionContent.addClass('hide');
                 $confirmationModal.modal('show');
             });
-            
+
             $rejectionButton.on('click', function(e) {
                 e.preventDefault();
                 $rejectionContent.removeClass('hide');
