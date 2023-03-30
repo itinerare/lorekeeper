ea8b441063234e4fe4369f2291fd60cc2e91150e
itinerare <ad@itinerare.net>
2023-03-30 17:16:15 -0400

    fix(users): better checks on displayAlias - display no alias if there are no visible aliases

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -322,10 +322,10 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     public function getDisplayAliasAttribute()
     {
-        if (!config('lorekeeper.settings.require_alias') && !$this->attributes['has_alias']) return '(No Alias)';
+        if (!config('lorekeeper.settings.require_alias') && (!$this->attributes['has_alias'] || !$this->aliases()->visible()->count())) return '(No Alias)';
         if (!$this->hasAlias) return '(Unverified)';
-        if($this->primaryAlias) return $this->primaryAlias->displayAlias;
-        elseif ($this->aliases->count()) return $this->aliases()->visible()->first()->displayAlias;
+        if ($this->primaryAlias) return $this->primaryAlias->displayAlias;
+        elseif ($this->aliases()->visible()->count()) return $this->aliases()->visible()->first()->displayAlias;
     }
 
     /**
